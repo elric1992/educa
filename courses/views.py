5eecf38ac59cecbc6b41e6e53c7cf7c4c8cec04e
holic <elric2009@gmail.com>
2020-10-16 15:01:16 +1000

    page 431

--- courses/views.py
+++ courses/views.py
@@ -111,6 +111,7 @@ class ContentCreateUpdateView(TemplateResponseMixin, View):
             return redirect('module_content_list', self.module.id)
         return self.render_to_response({'form': form, 'object': self.obj})
 
+
 class ContentDeleteView(View):
 
     def post(self, request, id):
@@ -119,3 +120,11 @@ class ContentDeleteView(View):
         content.item.delete()
         content.delete()
         return redirect('module_content_list', module.id)
+
+
+class ModuleContentListView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/content_list.html'
+
+    def get(self, request, module_id):
+        module = get_object_or_404(Module, id=module_id, course__owner=request.user)
+        return self.render_to_response({'module': module})
