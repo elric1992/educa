5f4b35b7be46660a6e716703b14238e4a2217ca4
elric1992 <elric2009@gmail.com>
2020-11-08 12:26:23 +1000

    page 443

--- courses/views.py
+++ courses/views.py
@@ -6,9 +6,11 @@ from django.shortcuts import redirect, get_object_or_404
 from django.views.generic.base import TemplateResponseMixin, View
 from django.forms.models import modelform_factory
 from django.apps import apps
+from django.db.models import Count
+from django.views.generic.detail import DetailView
 from braces.views import CsrfExemptMixin, JSONRequestResponseMixin
 from .forms import ModuleFormSet
-from .models import Course, Module, Content
+from .models import Course, Module, Content, Subject
 
 
 class OwnerMixin(object):
@@ -145,3 +147,21 @@ class ContentOrderView(CsrfExemptMixin, JSONRequestResponseMixin, View):
         for id, order in self.request_json.items():
             Content.objects.filter(id=id, module__course__owner=request.user).update(order=order)
         return self.render_json_response({'saved': 'OK'})
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        courses = Course.objects.annotate(total_modules=Count('modules'))
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({'subjects': subjects, 'subject': subject, 'courses': courses})
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
